f2bb14bcef62cc6125fbd604b1cdfdd5402e33ae
Pablo <pabromengibar@gmail.com>
2020-05-28 14:49:38 +0200

    Todo lo que tengo

---
+++ components/Article.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="flex-item box">
+						<div class="image fit">
+							<!-- <img :src='"("+ img +")"' alt="" /> -->
+						</div>
+						<div class="content">
+							<h3>{{title}}</h3>
+              <p>{{category}}</p>
+							<p>{{resume}}</p>
+              <p>{{date}}</p>
+              <p>{{urlArticle}}</p>
+						</div>
+					</div>
+  </div>
+</template>
+<script>
+export default {
+  name:"Article",
+  data(){
+    return{
+
+    }
+  },
+  props: ["title", "urlArticle","date","resume","category"]
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,28 @@
     	<body>
 			<section id="three" class="wrapper">
 				<div class="inner flex flex-3">
-					<div class="flex-item box">
-						<div class="image fit">
-							<img src="images/pic02.jpg" alt="" />
-						</div>
-						<div class="content">
-							<h3>Consequat</h3>
-							<p>Placerat ornare. Pellentesque od sed euismod in, pharetra ltricies edarcu cas consequat.</p>
-						</div>
-					</div>
-					<div class="flex-item box">
-						<div class="image fit">
-							<img src="images/pic03.jpg" alt="" />
-						</div>
-						<div class="content">
-							<h3>Adipiscing</h3>
-							<p>Morbi in sem quis dui placerat Pellentesque odio nisi, euismod pharetra lorem ipsum.</p>
-						</div>
-					</div>
-					<div class="flex-item box">
-						<div class="image fit">
-							<img src="images/pic04.jpg" alt="" />
-						</div>
-						<div class="content">
-							<h3>Malesuada</h3>
-							<p>Nam dui mi, tincidunt quis, accu an porttitor, facilisis luctus que metus vulputate sem magna.</p>
-						</div>
-					</div>
+					<Article v-for="item in articles" :key="item.id">
+					</Article>	
 				</div>
 			</section>
 	</body>
   </div>
 </template>
+<script>
+import Article from '@/components/Article'
+export default {
+	name: "index",
+	data(){
+		return{
+			articles:[]
+		}
+	},
+	components:{
+		Article
+	},
+	async mounted(){
+		const response = await this.$axios.get('http://localhost:8080/articles');
+		this.articles = response.data
+	}
+}
+</script>
